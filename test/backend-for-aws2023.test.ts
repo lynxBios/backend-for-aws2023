bca136124f1b063e37599d250ad0826345ef181c
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-16 06:46:00 +0300

    init: start task-3 First API with AWS API Gateway and AWS Lambda

---
+++ test/backend-for-aws2023.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BackendForAws2023 from '../lib/backend-for-aws2023-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/backend-for-aws2023-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BackendForAws2023.BackendForAws2023Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
