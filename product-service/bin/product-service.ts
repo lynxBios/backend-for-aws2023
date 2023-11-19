2755df677429795a1d5a8a4a5c9cf2ed36eef6be
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-19 18:49:24 +0300

    feat: add lambda and apigateway creation logic

--- product-service/bin/product-service.ts
+++ product-service/bin/product-service.ts
@@ -1,21 +1,46 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ProductServiceStack } from '../lib/product-service-stack';
+import * as apiGateway from 'aws-cdk-lib/aws-apigatewayv2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import dotenv from 'dotenv';
+//import { ProductServiceStack } from '../lib/product-service-stack';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+dotenv.config();
 
 const app = new cdk.App();
-new ProductServiceStack(app, 'ProductServiceStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stack = new cdk.ProductServiceStack(app, id: 'ProductServiceStack', props: {
+  
+  env: { region: 'eu-central-1' },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const sharedLambdaProps: NodejsFunctionProps = {  
+  runtime: lambda.Runtime.NODEJS_18_X,
+  environment: {
+    PRODUCT_AWS_REGION: process.env.PRODUCT_AWS_REGION!,
+  }  
+};
+
+const getProductsList = new NodejsFunction(stack, id: 'GetProductsList', props: {
+  ...sharedLambdaProps,
+  functionName: 'GetProductsList',
+  entry: 'src/lambdas/getProductsList.ts',
+});
+
+const api = new apiGateway.HttpApi(stack, id: 'GetProductsListApi', props: {
+  corsPreflight: {
+    allowHeaders: ['*'],
+    allowMethods: [apiGateway.CorsHttpMethod.ANY],
+    allowOrigins: ['*'],
+  },
+});
+
+api.addRoutes( options: {
+  path: '/products',
+  methods: [apiGateway.HttpMethod.GET],
+  integration: new HttpLambdaIntegration(id: 'GetProductsListIntegration', getProductsList),
 });
