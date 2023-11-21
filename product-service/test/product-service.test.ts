1bdabd9b4fc181173c1b06f63170c3b30f82b31b
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-21 06:18:18 +0300

    feat: add product-service.test.ts

--- product-service/test/product-service.test.ts
+++ product-service/test/product-service.test.ts
@@ -1,17 +1,60 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as ProductService from '../lib/product-service-stack';
+import { handler } from '../src/lambdas/getProductsList';
+import { buildResponse } from '../src/lambdas/utils';
+import { APIGatewayEvent } from 'aws-lambda';
+import { products } from '../src/lambdas/mocks';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/product-service-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ProductService.ProductServiceStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('handler', () => {
+  test('should return a successful response with products when productId is provided', async () => {
+    const event: APIGatewayEvent = {
+      pathParameters: {
+        productId: '1'
+      }
+    };
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const response = await handler(event);
+
+    expect(response).toEqual(buildResponse({ statusCode: 200, body: { product: products[0] } }));
+  });
+
+  test('should return a 400 error response when productId is not provided', async () => {
+    const invalidProductId = ""
+    const event: APIGatewayEvent = {
+      pathParameters: {
+        productId: invalidProductId
+      }
+    };
+
+    const response = await handler(event);
+
+    expect(response).toEqual(buildResponse({ statusCode: 400, body: { message: 'productId is required' } }));
+  });
+
+  test('should return a 404 error response when product is not found', async () => {
+    const event: APIGatewayEvent = {
+      pathParameters: {
+        productId: '999'
+      }
+    };
+
+    const response = await handler(event);
+
+    expect(response).toEqual(buildResponse({ statusCode: 404, body: { message: 'Product not found' } }));
+  });
+
+  test('should return a 500 error response when an error occurs', async () => {
+    const event: APIGatewayEvent = {
+      pathParameters: {
+        productId: '1'
+      }
+    };
+
+    // Mock the buildResponse function to throw an error
+    jest.spyOn(buildResponse, 'buildResponse').mockImplementation(() => {
+      throw new Error('Test error');
+    });
+
+    const response = await handler(event);
+
+    expect(response).toEqual(buildResponse({ statusCode: 500, body: { message: 'Test error' } }));
+  });
 });
