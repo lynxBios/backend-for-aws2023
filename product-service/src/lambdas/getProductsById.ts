4db8751669e1fef7afe400ee2db9f57fec2a28d6
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-22 21:19:42 +0300

    fix: format of returning object

--- product-service/src/lambdas/getProductsById.ts
+++ product-service/src/lambdas/getProductsById.ts
@@ -1,18 +1,20 @@
-import { APIGatewayEvent } from 'aws-lambda';
-import { products } from './mocks';
+//import { APIGatewayEvent } from 'aws-lambda';
+//import { products } from './mocks';
 import { buildResponse } from './utils';
+import { findItem } from "../services/productsService";
 
-const handler = async (event: APIGatewayEvent) => {
+const handler = async (event: { pathParameters: { productId: string | undefined } }) => {
   try {
-    const { productId } = event.pathParameters || {};
+
+    const productId  = event.pathParameters.productId || null;
     if (!productId) {
       return buildResponse({ statusCode: 400, body: { message: 'productId is required' } });
     }
-    const product = products.find(({ id }) => id === productId);
+    const product = findItem( productId );
     if (!product) {
       return buildResponse({ statusCode: 404, body: { message: 'Product not found' } });
     }
-    return buildResponse({ statusCode: 200, body: { product } });
+    return buildResponse({ statusCode: 200, body: product });
   } catch (error: any) {
     return buildResponse({ statusCode: 500, body: { message: error.message || '' } });
   }
