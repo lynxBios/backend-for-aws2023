5cbadf8d95fc3769ce1c11b03ab2e2c12483155c
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-26 23:17:20 +0300

    feat: integrate the getProductsList lambda to return via GET /products request a list of products from the DynamoDB

---
+++ product-service/src/lambdas/createProduct.ts
@@ -0,0 +1,33 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuidv4 } from '@uuid';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  try {
+    const { title, description, price } = JSON.parse(event.body || '');
+    
+    const params: DynamoDB.DocumentClient.PutItemInput = {
+      TableName: 'products',
+      Item: {
+        id: uuidv4(),
+        title,
+        description,
+        price,
+      },
+    };
+
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ message: 'Product created successfully' }),
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message || 'Internal server error' }),
+    };
+  }
+};
--- product-service/src/lambdas/getProductsList.ts
+++ product-service/src/lambdas/getProductsList.ts
@@ -1,10 +1,32 @@
 import { buildResponse } from "./utils";
-//import { APIGatewayEvent } from "aws-lambda";
-import { findAll } from "../services/productsService";
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const scan = async (tableName: string) => {
+  const scanResults = await dynamo.scan({
+    TableName: tableName
+  }).promise();
+
+  return scanResults.Items;
+}
 
 export const handler = async () => {
   try {
-    return buildResponse({ statusCode: 200, body: findAll() });
+    const scanResultsProducts = await scan(process.env.PRODUCTS_TABLE_NAME!);
+    const scanResultsStocks = await scan(process.env.STOCKS_TABLE_NAME!);
+
+    const combinedResults = scanResultsProducts.map((product: any) => {
+    const stock = scanResultsStocks.find((stock: any) => stock.product_id === product.id);
+    return {
+      id: product.id,
+      count: stock.count,
+      price: product.price,
+      title: product.title,
+      description: product.description,
+    };
+  });
+  return buildResponse({ statusCode: 200, body: combinedResults });
   } catch (error: any) {
     return buildResponse({ statusCode: 500, body: { message: error.message || "" }});
   }
