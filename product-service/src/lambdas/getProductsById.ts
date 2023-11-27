02957f5835b46e5e7896abd54b12e9297a10d48a
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-27 09:53:07 +0300

    feat: integrate the getProductsById lambda to return a product from the DynamoDB

--- product-service/src/lambdas/getProductsById.ts
+++ product-service/src/lambdas/getProductsById.ts
@@ -1,21 +1,33 @@
-//import { APIGatewayEvent } from 'aws-lambda';
-//import { products } from './mocks';
 import { buildResponse } from './utils';
-import { findItem } from "../services/productsService";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, TransactGetCommand } from "@aws-sdk/lib-dynamodb";
 
-const handler = async (event: { pathParameters: { productId: string | undefined } }) => {
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+
+const handler = async (
+  event: APIGatewayProxyEventV2,
+): Promise<APIGatewayProxyResultV2> => {
   try {
-
-    const productId  = event.pathParameters.productId || null;
+    const { productId } = event.pathParameters || {};    
     if (!productId) {
       return buildResponse({ statusCode: 400, body: { message: 'productId is required' } });
     }
-    const product = findItem( productId );
-    if (!product) {
+    const params = {
+      TableName: 'products',
+      Key: {
+        id: { S: productId },
+      },
+    };
+    const command = new GetCommand(params);
+    const { Item } = await client.send(command);
+    if (!Item) {
       return buildResponse({ statusCode: 404, body: { message: 'Product not found' } });
     }
-    return buildResponse({ statusCode: 200, body: product });
-  } catch (error: any) {
+    return buildResponse({ statusCode: 200, body: Item });
+  } catch (error) {
     return buildResponse({ statusCode: 500, body: { message: error.message || '' } });
   }
 }
