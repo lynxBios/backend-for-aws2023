461b692ad956c0ed82b9c69c2ec83ea3ca9fed04
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-30 19:15:19 +0300

    feat: all lambdas do console.log for each incoming requests and their arguments

--- product-service/src/lambdas/getProductsById.ts
+++ product-service/src/lambdas/getProductsById.ts
@@ -7,8 +7,10 @@ const handler = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> => {
   try {
+    console.log('event', event);
     const { productId } = event.pathParameters || {};     
     if (!productId) {
+      console.log('no productId');
       return buildResponse({ statusCode: 400, body: { message: 'productId is required' } });
     }
     const params = {
@@ -20,6 +22,7 @@ const handler = async (
     const command = new GetItemCommand(params);
     const { Item } = await client.send(command) as { Item: any };
     if (!Item) {
+      console.log('Product not found');
       return buildResponse({ statusCode: 404, body: { message: 'Product not found' } });
     }
     return buildResponse({ statusCode: 200, body: Item });
--- product-service/src/lambdas/getProductsList.ts
+++ product-service/src/lambdas/getProductsList.ts
@@ -12,21 +12,22 @@ const scan = async (tableName: string) => {
 }
 
 export const handler = async () => {
-  try {
+  console.log('GET all products');
+  try {    
     const scanResultsProducts = await scan(process.env.PRODUCTS_TABLE_NAME!);
     const scanResultsStocks = await scan(process.env.STOCKS_TABLE_NAME!);
 
     const combinedResults = scanResultsProducts.map((product: any) => {
-    const stock = scanResultsStocks.find((stock: any) => stock.product_id === product.id);
-    return {
-      id: product.id,
-      count: stock.count,
-      price: product.price,
-      title: product.title,
-      description: product.description,
-    };
-  });
-  return buildResponse({ statusCode: 200, body: combinedResults });
+      const stock = scanResultsStocks.find((stock: any) => stock.product_id === product.id);
+      return {
+        id: product.id,
+        count: stock.count,
+        price: product.price,
+        title: product.title,
+        description: product.description,
+      };
+    });
+    return buildResponse({ statusCode: 200, body: combinedResults });
   } catch (error: any) {
     return buildResponse({ statusCode: 500, body: { message: error.message || "" }});
   }
