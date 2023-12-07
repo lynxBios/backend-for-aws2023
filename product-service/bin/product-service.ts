cf538919afc0b3809458b0a22a808abcbe35b8ff
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-12-07 12:55:34 +0300

    feat: grant read/write data rights to lambdas

--- product-service/bin/product-service.ts
+++ product-service/bin/product-service.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
@@ -25,6 +26,18 @@ const createLambda = (name: string, props: NodejsFunctionProps ) => new NodejsFu
   ...props,
 });
 
+const productsTable = dynamodb.Table.fromTableName(
+  stack,
+  "ProductsTable",
+  process.env.PRODUCTS_TABLE_NAME as string
+);
+
+const stocksTable = dynamodb.Table.fromTableName(
+  stack,
+  "StocksTable",
+  process.env.STOCKS_TABLE_NAME as string
+);
+
 const getProductsList = createLambda('GetProductsListLambda', {
   entry: 'src/lambdas/getProductsList.ts',
   functionName: 'getProductsList',
@@ -47,6 +60,18 @@ const api = new apiGateway.HttpApi(stack, 'ProductsApiGateway', {
     allowOrigins: ['*'],
   },
 });
+
+const grants = [
+  productsTable.grantReadData(getProductsList),
+  stocksTable.grantReadData(getProductsList),
+
+  productsTable.grantReadData(getProductsById),
+  stocksTable.grantReadData(getProductsById),
+
+  productsTable.grantWriteData(createProduct),
+  stocksTable.grantWriteData(createProduct),
+];
+grants.forEach((grant) => grant.assertSuccess());
 
 api.addRoutes({
   path: `/${BASE_URL}`,
--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -4,12 +4,6 @@ import { Construct } from 'constructs';
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
